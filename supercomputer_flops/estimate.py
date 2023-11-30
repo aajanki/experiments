4e89275709d67acea07d5b1fdd3a1c179df60638
Antti Ajanki <antti.ajanki@iki.fi>
2023-11-30 20:27:24 +0200

    Plot yearly calculation times

--- supercomputer_flops/estimate.py
+++ supercomputer_flops/estimate.py
@@ -21,7 +21,41 @@ print(f'exp(slope): {np.exp(regress.slope)}')
 
 df['yhat'] = np.exp(regress.intercept + df.index*regress.slope)
 
+
+### optimize
+
+#df2 = 
+
+
+### plots
+
+plt.figure()
 plt.plot(df['Year'], df['Floating-Point Operations per Second (GFLOPS)'])
 plt.plot(df['Year'], df['yhat'], 'b--')
-plt.yscale('log',base=10)
+plt.yscale('log', base=10)
+
+
+plt.figure(figsize=(8, 4))
+
+# How long does it take to get an answer to a calculation that takes
+# 10 million years in year 0 computer if we first wait for n years and
+# then run the computation on that year's model?
+
+a = 10_000_000
+x = 1.0/np.exp(regress.slope)
+n = np.arange(50)
+total_time = n + a * x**n
+
+plt.step(n, total_time, where='post')
+plt.yscale('log', base=10)
+plt.gca().set_yticks([100, 1000, 10_000, 100_000, 1_000_000, 10_000_000])
+plt.gca().set_yticklabels(['100', '1 000', '10 000', '100 000', '1 milj', '10 milj'])
+plt.xlabel('Odotusaika ennen laskennan aloittamista (vuosia)')
+plt.ylabel('Vastaukseen saamiseen kuluva aika (vuosia)')
+plt.gca().spines['top'].set_visible(False)
+plt.gca().spines['right'].set_visible(False)
+
+plt.savefig('laskenta-aika.png', dpi=100)
+
 plt.show()
+
